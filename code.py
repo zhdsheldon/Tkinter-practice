c04ce859bd22fe7c99a0886fe218a4e37bc85e3c
zhdsheldon <34999276+zhdsheldon@users.noreply.github.com>
2022-08-02 10:46:42 +1000

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,14 @@
+#program 1
 import tkinter as tk
 
 window = tk.Tk()
-greeting = tk.Label(text="neuroscience is fascinating")
+greeting = tk.Label(text="neuroscience is fascinating") #A widget used to display text on the screen
 greeting.pack()
 
 window.mainloop()
+
+#Widgets are the bread and butter of the Python GUI framework Tkinter. 
+#They’re the elements through which users interact with your program.
+#Each widget in Tkinter is defined by a class.
+#Reference:https://realpython.com/python-gui-tkinter/
+
